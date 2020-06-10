5a5f582ccdbd813a26df846ce2eda61b349e6ce7
“Kaustavi <kaustavisen8@gmail.com>
2020-06-10 21:53:03 +0530

    Added:Repeat till winning position 100 with undershooting restricted but overshooting allowed

--- snakeLadderSimulator.sh
+++ snakeLadderSimulator.sh
@@ -1,34 +1,51 @@
 #! /bin/bash -x
 
 playerOnePosition=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
 rollDice(){
 	local roll=$((RANDOM%6+1))
 	echo $roll
 }
 genPlayOptions(){
 	local roll=$(rollDice)
-	NO_PLAY=0
-	LADDER=1
-	SNAKE=2
 	local option=$((RANDOM%3))
 	local move=-1
 	case $option in
 		$NO_PLAY) 
-			echo "NO_PLAY"
 			move=0
 			;;
 
 		$LADDER) 
-			echo "LADDER"
 			move=$roll
 			;;
 		   
 		$SNAKE)
-			echo "SNAKE"
 			move=-$roll
 			;;
 	esac
 	echo $move
 }
-move=$(genPlayOptions)
-echo $move
+movePlayer(){
+	local current=$1
+	local currentInit=$current
+	local move=$(genPlayOptions)
+	current=$(($current+$move))
+	if [ $current -lt 0 ]
+	then
+		current=0
+	fi	
+	echo $current
+}
+playGame(){
+	local countTurns=0
+	local current=$1
+	while [ $current -ne 100 ]
+	do  
+		current=$(movePlayer $current)
+	done
+	echo $current
+	echo "Player wins!"
+}
+playGame $playerOnePosition
