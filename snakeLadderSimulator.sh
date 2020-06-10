16a50559a715ccac5cb2ab706aa4c74a65052271
“Kaustavi <kaustavisen8@gmail.com>
2020-06-10 21:48:52 +0530

    Added:Single move of first player

--- snakeLadderSimulator.sh
+++ snakeLadderSimulator.sh
@@ -5,5 +5,30 @@ rollDice(){
 	local roll=$((RANDOM%6+1))
 	echo $roll
 }
-roll=$(rollDice)
-echo $roll
+genPlayOptions(){
+	local roll=$(rollDice)
+	NO_PLAY=0
+	LADDER=1
+	SNAKE=2
+	local option=$((RANDOM%3))
+	local move=-1
+	case $option in
+		$NO_PLAY) 
+			echo "NO_PLAY"
+			move=0
+			;;
+
+		$LADDER) 
+			echo "LADDER"
+			move=$roll
+			;;
+		   
+		$SNAKE)
+			echo "SNAKE"
+			move=-$roll
+			;;
+	esac
+	echo $move
+}
+move=$(genPlayOptions)
+echo $move
